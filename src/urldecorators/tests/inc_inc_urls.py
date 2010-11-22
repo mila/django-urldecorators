42e6ea0bc249863a158647a22381736397493538
Miloslav Pojman <miloslav.pojman@gmail.com>
2010-11-22 10:50:14 +0100

    Wrapped long lines.

--- src/urldecorators/tests/inc_inc_urls.py
+++ src/urldecorators/tests/inc_inc_urls.py
@@ -6,7 +6,3 @@ urlpatterns = patterns('urldecorators.tests.views',
     url(r'^args/(\d+)/(\d+)/$', 'sample_view'),
     url(r'^kwargs/(?P<inc_inc_arg1>\d+)/(?P<inc_inc_arg2>\d+)/$', 'sample_view'),
 )
-
-
-
-
